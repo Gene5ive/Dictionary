7cc01b1396471575757197143f27a1a62d078624
Guest User <student@epicodus.com>
2015-05-01 09:30:18 -0700

    wrinting first sepc for init and attr_reader which fails as expected.

---
+++ lib/dictionary.rb
@@ -0,0 +1,14 @@
+class Definition
+
+  # @@definition_info = []
+  #
+  # attr_reader(:first, :second, :third)
+  #
+  # define_method(:initialize) do |first, second, third|
+  #   @first = first
+  #   @second = second
+  #   @third = third
+  #   @id = @@definition_info.length + 1
+  # end
+  #
+end
---
+++ spec/dictionary_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require 'dictionary'
+require 'pry'
+
+describe (Definition) do
+
+  describe('#initialize', '#attr_reader') do
+    it('returns the list of definitions for the word') do
+      test_definition = Definition.new("short", "not-long", "little")
+      expect(test_definition.first).to(eq("short"))
+      expect(test_definition.first).to(eq("not-long"))
+      expect(test_definition.first).to(eq("little"))
+    end
+  end
+end
